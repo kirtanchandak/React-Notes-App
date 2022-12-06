b551e892d3e18cc232707c803433dfc3d1aeac43
kirtanchandak <themarketingkirtan@gmail.com>
2022-12-06 19:49:54 +0530

    solving deployment error

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
 import NotesList from "./components/NotesList";
 import { nanoid } from "nanoid";
-import Note from "./components/Note";
 import Header from "./components/Header";
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
 import Auth from "./Auth";
 import Account from "./Account";
-
-// function App() {
-//   const [notes, setNotes] = useState([
-//     {
-//       id: nanoid(),
-//       text: "This is my first note",
-//       date: "21/06/22",
-//     },
-//     {
-//       id: nanoid(),
-//       text: "This is my second note",
-//       date: "22/06/22",
-//     },
-//   ]);
-
-//   const addNote = (text) => {
-//     const date = new Date();
-//     const newNote = {
-//       id: nanoid(),
-//       text: text,
-//       date: date.toLocaleDateString("en-GB"),
-//     };
-//     const newNotes = [...notes, newNote];
-//     setNotes(newNotes);
-//   };
-
-//   const deleteNote = (id) => {
-//     const newNotes = notes.filter((note) => note.id !== id);
-//     setNotes(newNotes);
-//   };
-//   return (
-{
-  /* <div className="max-w-[960px] ml-auto mr-auto px-4">
-  <Header />
-  <NotesList
-    notes={notes}
-    handleAddNote={addNote}
-    handleDeleteNote={deleteNote}
-  />
-</div>; */
-}
-//   );
-// }
 
 // export default App;
 export default function App() {
