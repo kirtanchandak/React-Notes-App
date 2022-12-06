b19241c1e742aebcd2bca84ad146a3b820ebef16
kirtanchandak <themarketingkirtan@gmail.com>
2022-12-06 19:40:39 +0530

    testing login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import NotesList from "./components/NotesList";
-import { useState } from "react";
 import { nanoid } from "nanoid";
 import Note from "./components/Note";
 import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+import Auth from "./Auth";
+import Account from "./Account";
 
-function App() {
+// function App() {
+//   const [notes, setNotes] = useState([
+//     {
+//       id: nanoid(),
+//       text: "This is my first note",
+//       date: "21/06/22",
+//     },
+//     {
+//       id: nanoid(),
+//       text: "This is my second note",
+//       date: "22/06/22",
+//     },
+//   ]);
+
+//   const addNote = (text) => {
+//     const date = new Date();
+//     const newNote = {
+//       id: nanoid(),
+//       text: text,
+//       date: date.toLocaleDateString("en-GB"),
+//     };
+//     const newNotes = [...notes, newNote];
+//     setNotes(newNotes);
+//   };
+
+//   const deleteNote = (id) => {
+//     const newNotes = notes.filter((note) => note.id !== id);
+//     setNotes(newNotes);
+//   };
+//   return (
+{
+  /* <div className="max-w-[960px] ml-auto mr-auto px-4">
+  <Header />
+  <NotesList
+    notes={notes}
+    handleAddNote={addNote}
+    handleDeleteNote={deleteNote}
+  />
+</div>; */
+}
+//   );
+// }
+
+// export default App;
+export default function App() {
   const [notes, setNotes] = useState([
     {
       id: nanoid(),
@@ -33,16 +80,35 @@ function App() {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
   };
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <div className="max-w-[960px] ml-auto mr-auto px-4">
-      <Header />
-      <NotesList
-        notes={notes}
-        handleAddNote={addNote}
-        handleDeleteNote={deleteNote}
-      />
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth />
+      ) : (
+        <div>
+          <Account key={session.user.id} session={session} />
+          <div className="max-w-[960px] ml-auto mr-auto px-4">
+            <Header />
+            <NotesList
+              notes={notes}
+              handleAddNote={addNote}
+              handleDeleteNote={deleteNote}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
